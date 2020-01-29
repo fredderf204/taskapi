e126a71fbe8450a1ff471418a5f2133142c7b1ce
Michael Friedrich <mifrie@microsoft.com>
2020-01-29 11:48:30 +1100

    fixed connection pooling

--- api.go
+++ api.go
@@ -21,6 +21,8 @@ var (
 	host     string
 	//Collection is the MongoDB collection
 	Collection *mgo.Collection
+	//Session handles the session for MongoDB
+	Session *mgo.Session
 )
 
 //init defines variables used in func main
@@ -29,6 +31,32 @@ func init() {
 	password = os.Getenv("AZURE_DATABASE_PASSWORD")
 	username = os.Getenv("AZURE_DATABASE_USERNAME")
 	host = os.Getenv("AZURE_DATABASE_HOST")
+
+	// DialInfo holds options for establishing a session with a MongoDB cluster.
+	dialInfo := &mgo.DialInfo{
+		Addrs:    []string{fmt.Sprintf("%s.documents.azure.com:10255", host)},
+		Timeout:  60 * time.Second,
+		Database: database,
+		Username: username,
+		Password: password,
+		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+			return tls.Dial("tcp", addr.String(), &tls.Config{})
+		},
+	}
+
+	// Create a session which maintains a pool of socket connections to our MongoDB.
+	session, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		fmt.Printf("Can't connect to mongo, go error %v\n", err)
+		os.Exit(1)
+	}
+
+	//defer session.Close()
+
+	// SetSafe changes the session safety mode.
+	session.SetSafe(&mgo.Safe{})
+
+	Session = session
 }
 
 //Task model
@@ -54,39 +82,19 @@ func SetupRouter() *gin.Engine {
 
 //main function
 func main() {
-	// DialInfo holds options for establishing a session with a MongoDB cluster.
-	dialInfo := &mgo.DialInfo{
-		Addrs:    []string{fmt.Sprintf("%s.documents.azure.com:10255", host)},
-		Timeout:  60 * time.Second,
-		Database: database,
-		Username: username,
-		Password: password,
-		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
-			return tls.Dial("tcp", addr.String(), &tls.Config{})
-		},
-	}
-
-	// Create a session which maintains a pool of socket connections to our MongoDB.
-	Session, err := mgo.DialWithInfo(dialInfo)
-	if err != nil {
-		fmt.Printf("Can't connect to mongo, go error %v\n", err)
-		os.Exit(1)
-	}
-
-	defer Session.Close()
-
-	// SetSafe changes the session safety mode.
-	Session.SetSafe(&mgo.Safe{})
-
-	// get collection
-	Collection = Session.DB(database).C("tasks")
-
 	router := SetupRouter()
 	router.Run()
 }
 
 //getAllTasks gets all Tasks from the collection
 func getAllTasks(c *gin.Context) {
+	// grab session from connection pool
+	sessionCopy := Session.Copy()
+	defer sessionCopy.Close()
+
+	// connect to collection
+	Collection = sessionCopy.DB(database).C("tasks")
+
 	result := make([]Task, 0, 15)
 	err := Collection.Find(nil).All(&result)
 	if err != nil {
@@ -117,22 +125,6 @@ func getTaskbyID(c *gin.Context) {
 
 //createNewTask creates a new task
 func createNewTask(c *gin.Context) {
-	/*if len(c.Query("description")) == 0 {
-		log.Print("query paramter description missing")
-		c.JSON(400, gin.H{"error": "query paramter description missing"})
-		return
-	}
-	if len(c.Query("title")) == 0 {
-		log.Print("query paramter title missing")
-		c.JSON(400, gin.H{"error": "query paramter title missing"})
-		return
-	}
-	err := Collection.Insert(&Task{
-		Completed:   false,
-		Description: c.Query("description"),
-		Duedate:     time.Now().AddDate(0, 0, 7).Format(time.RFC3339),
-		Title:       c.Query("title"),
-	})*/
 	if len(c.PostForm("title")) == 0 {
 		log.Print("title missing")
 		c.JSON(400, gin.H{"error": "title missing"})
